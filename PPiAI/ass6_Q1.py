fb116a4abe227453ced73a5e015681a798d71d33
Sinothi <66753379+phyllsmoyo@users.noreply.github.com>
2021-08-14 23:09:05 +0200

    Create ass6_Q1.py

---
+++ PPiAI/ass6_Q1.py
@@ -0,0 +1,9 @@
+# Question 1
+print("What is your number:")
+num_str = str(input())
+# Test if condition is met and print valid
+if len(num_str) == 5:
+  print ("Valid")
+# if condition is met and print invalid
+else:
+  print ("Invalid")
