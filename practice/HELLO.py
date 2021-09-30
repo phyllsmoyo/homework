819d67537b1115e20b477b70e0c4dbf0e363eedf
Phillip Sinothi <phyllsmoyo@gmail.com>
2021-09-30 07:55:34 +0200

    "Ass7 old updates"

--- practice/HELLO.py
+++ practice/HELLO.py
@@ -1,32 +1,9 @@
-# Reading files in python
+filename = input("What is the filename: ")
+if filename == "" or not filename.endswith(".txt"):
+    print("Invalid filename.")
+    exit(0)
+else:
+    # Open a file for writing and create it if it does not exist
+    f = open(filename, "w+")
 
-# Reading the number of lines
-fhand = open("practice/mbox.txt")
-count = 0
-
-for line in fhand:
-    count += 1
-print(f"Line Count {count}")
-
-# Read the whole file
-
-fhand = open("practice/mbox-short.txt")
-inp = fhand.read()
-print(len(inp))
-
-print(inp[0:20])
-
-# Searching through the file
-fhand = open("practice/mbox-short.txt")
-for line in fhand:
-    line = line.strip()
-    if line.startswith("From:"):
-        print(line)
-
-##Skipping with Continue
-fhand = open("practice/mbox-short.txt")
-for line in fhand:
-    line = line.strip()
-    if not line.startswith("From: "):
-        continue
-    print(line)
+    f.write("id,enroll_year,name,stream,student,faculty,source_file_path")
